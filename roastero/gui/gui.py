8a629d2bd3b8e33cc4254d817872a107ff49a99c
lodge93 <mds4680@rit.edu>
2015-01-10 09:10:10 -0500

    Added custom gui buttons

--- roastero/gui/gui.py
+++ roastero/gui/gui.py
@@ -1,5 +1,6 @@
 # Import necessary modules.
 import tkinter as tk
+from tkinter import ttk
 
 # Import classes
 from .log import logTab
@@ -9,13 +10,17 @@ from .roast import roastTab
 class gui:
     def __init__(self):
         # Define class variables.
-        self.bgColor = "white"
+        self.bgColor = "#2e3138"
+        #self.bgColor = "white"
 
         # Define root window.
         self.root = tk.Tk()
         self.root.title("Roastero")
         self.root.config(bg=self.bgColor)
         self.root.geometry("800x500")
+        
+        # Define images to be used.
+        self.miniButton = tk.PhotoImage(file="../images/button.gif")
 
         # Create the widgets to fill root window.
         self.createWidgets()
@@ -35,15 +40,41 @@ class gui:
         self.log.grid(row=1, column=0, sticky="nsew")
 
         # Create tab buttons.
-        self.roastButton = tk.Button(self.root, text="Roast", command=lambda: self.show_frame(self.roast))
+        self.roastButton = self.makeTabButton("Roast", self.roast)
         self.roastButton.grid(row=0, column=0)
-        self.recipeButton = tk.Button(self.root, text="Recipe", command=lambda: self.show_frame(self.recipes))
+        self.recipeButton = self.makeTabButton("Recipes", self.recipes)
         self.recipeButton.grid(row=0, column=1)
-        self.logButton = tk.Button(self.root, text="Log", command=lambda: self.show_frame(self.log))
+        self.logButton = self.makeTabButton("Log", self.log)
         self.logButton.grid(row=0, column=2)
 
         # Show the default tab.
-        self.show_frame(self.roast)
+        self.showFrame(self.roast)
 
-    def show_frame(self, tab):
+    def showFrame(self, tab):
         tab.tkraise()
+
+    def makeTabButton(self, buttonText, showFrame):
+        return tk.Button(self.root,
+                        activebackground="#f00",
+                        activeforeground=self.bgColor,
+                        anchor="center",
+                        background=self.bgColor,
+                        bg=self.bgColor,
+                        borderwidth=-2,
+                        bd=-2,
+                        disabledforeground=self.bgColor,
+                        foreground=self.bgColor,
+                        fg=self.bgColor,
+                        highlightbackground=self.bgColor,
+                        highlightcolor=self.bgColor,
+                        highlightthickness=0,
+                        justify="center",
+                        repeatdelay=10,
+                        text=buttonText,
+                        command=lambda: self.showFrame(showFrame),
+                        image=self.miniButton,
+                        compound="center",
+                        height=36,
+                        width=140,
+                        padx=3,
+                        pady=3)
--- roastero/gui/roast.py
+++ roastero/gui/roast.py
@@ -2,5 +2,5 @@ import tkinter as tk
 class roastTab(tk.Frame):
     def __init__(self, parent, root):
         tk.Frame.__init__(self,parent)
-        label = tk.Label(self, text="roast")
+        label = tk.Label(self, text="roast", bg=root.bgColor)
         label.pack(pady=10,padx=10)
