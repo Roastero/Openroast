baf9cd9071ab4022e43c84e133058210979a96bd
CCoffie <CalebCoffie@gmail.com>
2015-01-13 13:42:08 -0500

    Beginning stages of roast temperature graph

--- roastero/gui/RoastTab.py
+++ roastero/gui/RoastTab.py
@@ -1,6 +1,48 @@
+import matplotlib
+matplotlib.use('Qt5Agg')
 from PyQt5.QtWidgets import *
+from matplotlib.figure import Figure
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.backends.backend_qt5agg import (
+    FigureCanvasQTAgg as FigureCanvas,
+    NavigationToolbar2QT as NavigationToolbar)
+
 class RoastTab(QWidget):
     def __init__(self):
         super(RoastTab, self).__init__()
-        button = QPushButton("Test", self)
-        button.setObjectName("mainButton")
+
+        self.create_ui()
+
+    def create_ui(self):
+        self.create_graph()
+        self.graph_draw()
+
+    def create_graph(self):
+        self.graphWidget = QWidget(self)
+
+        self.graphFigure = Figure((5.0, 4.0), dpi=100)
+        self.graphCanvas = FigureCanvas(self.graphFigure)
+        self.graphCanvas.setParent(self.graphWidget)
+        #self.graphCanvas.setFocusPolicy(Qt.StrongFocus)
+        self.graphCanvas.setFocus()
+
+        self.graphToolbar = NavigationToolbar(self.graphCanvas, self.graphWidget)
+
+        self.graphCanvas.mpl_connect('key_press_event', self.graph_on_key_press)
+
+        graphVerticalBox = QVBoxLayout()
+        graphVerticalBox.addWidget(self.graphCanvas)
+        graphVerticalBox.addWidget(self.graphToolbar)
+        self.graphWidget.setLayout(graphVerticalBox)
+
+    def graph_on_key_press(self, event):
+        print('you pressed', event.key)
+        # implement the default mpl key press events described at
+        # http://matplotlib.org/users/navigation_toolbar.html#navigation-keyboard-shortcuts
+        key_press_handler(event, self.graphCanvas, self.graphToolbar)
+
+    def graph_draw(self):
+        self.graphFigure.clear()
+        self.graphAxes = self.graphFigure.add_subplot(111)
+        self.graphAxes.plot([1,2,3])
+        self.graphCanvas.draw()
