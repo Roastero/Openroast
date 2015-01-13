fffc3b308c7e66f9b955080599a3d2e0a0ab1138
CCoffie <CalebCoffie@gmail.com>
2015-01-13 14:36:35 -0500

    More work on Roast tab graph with a random data source. Still working on the graph formatting.

--- roastero/gui/RoastTab.py
+++ roastero/gui/RoastTab.py
@@ -1,7 +1,12 @@
+import random
+import datetime
 import matplotlib
 matplotlib.use('Qt5Agg')
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 from matplotlib.figure import Figure
+import matplotlib.animation as animation
+import matplotlib.dates as mdates
 from matplotlib.backend_bases import key_press_handler
 from matplotlib.backends.backend_qt5agg import (
     FigureCanvasQTAgg as FigureCanvas,
@@ -10,11 +15,13 @@ from matplotlib.backends.backend_qt5agg import (
 class RoastTab(QWidget):
     def __init__(self):
         super(RoastTab, self).__init__()
-
+        self.graphXValueList = []
+        self.graphYValueList = []
         self.create_ui()
 
     def create_ui(self):
         self.create_graph()
+        animateGraph = animation.FuncAnimation(self.graphFigure, self.graph_draw, interval=1000)
         self.graph_draw()
 
     def create_graph(self):
@@ -23,7 +30,7 @@ class RoastTab(QWidget):
         self.graphFigure = Figure((5.0, 4.0), dpi=100)
         self.graphCanvas = FigureCanvas(self.graphFigure)
         self.graphCanvas.setParent(self.graphWidget)
-        #self.graphCanvas.setFocusPolicy(Qt.StrongFocus)
+        self.graphCanvas.setFocusPolicy(Qt.StrongFocus)
         self.graphCanvas.setFocus()
 
         self.graphToolbar = NavigationToolbar(self.graphCanvas, self.graphWidget)
@@ -41,8 +48,15 @@ class RoastTab(QWidget):
         # http://matplotlib.org/users/navigation_toolbar.html#navigation-keyboard-shortcuts
         key_press_handler(event, self.graphCanvas, self.graphToolbar)
 
-    def graph_draw(self):
+    def graph_draw(self, *args, **kwargs):
+        self.graph_get_data()
         self.graphFigure.clear()
         self.graphAxes = self.graphFigure.add_subplot(111)
-        self.graphAxes.plot([1,2,3])
+        self.graphAxes.plot_date(self.graphXValueList, self.graphYValueList, '-')
         self.graphCanvas.draw()
+
+    def graph_get_data(self):
+        currentTime = datetime.datetime.now()
+        randomNumber = random.randint(1, 500)
+        self.graphXValueList.append(matplotlib.dates.date2num(currentTime))
+        self.graphYValueList.append(randomNumber)
