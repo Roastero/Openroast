0d859c19431c21582ee05939b12d4aff0df3f4ce
CCoffie <CalebCoffie@gmail.com>
2015-04-02 17:36:46 -0400

    Slowly working on the Roast tab

--- roastero/modules/gui/RecipesTab.py
+++ roastero/modules/gui/RecipesTab.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
+import os, json
 class RecipesTab(QWidget):
     def __init__(self):
         super(RecipesTab, self).__init__()
@@ -24,7 +25,7 @@ class RecipesTab(QWidget):
 
 
     def create_recipe_browser(self):
-        self.model = QFileSystemModel()
+        self.model = RecipeModel()
         self.model.setRootPath('./recipes')
         #model.setIconProvider()
         self.recipeBrowser = QTreeView()
@@ -37,11 +38,50 @@ class RecipesTab(QWidget):
         self.recipeBrowser.setAnimated(True)
         self.recipeBrowser.setIndentation(0)
         self.recipeBrowser.setSortingEnabled(True)
+        self.recipeBrowser.setColumnHidden(0, True)
         self.recipeBrowser.setColumnHidden(1, True)
         self.recipeBrowser.setColumnHidden(2, True)
         self.recipeBrowser.setColumnHidden(3, True)
 
+        self.recipeBrowser.clicked.connect(self.on_recipeBrowser_clicked)
+
     def create_recipe_window(self):
         self.recipeWindow = QPlainTextEdit()
-        text=open('./recipes/Favorites/test01').read()
+        text=open('./recipes/Favorites/test01.json').read()
         self.recipeWindow.setPlainText(text)
+
+    def on_recipeBrowser_clicked(self, index):
+        indexItem = self.model.index(index.row(), 0, index.parent())
+
+        filePath = self.model.filePath(indexItem)
+
+        # Allow single click expanding of folders
+        if os.path.isdir(filePath):
+            if self.recipeBrowser.isExpanded(indexItem):
+                self.recipeBrowser.collapse(indexItem)
+            else:
+                self.recipeBrowser.expand(indexItem)
+        # Handles when a file is clicked
+        else:
+            with open(filePath) as json_data:
+                recipeObject = json.load(json_data)
+            self.recipeWindow.setPlainText(recipeObject["roastName"])
+
+class RecipeModel(QFileSystemModel):
+    def columnCount(self, parent = QModelIndex()):
+        return super(RecipeModel, self).columnCount()+1
+
+    def data(self, index, role):
+        if index.column() == self.columnCount() - 1:
+            if role == Qt.DisplayRole:
+                filePath = self.filePath(index)
+                if os.path.isfile(filePath):
+                    with open(filePath) as json_data:
+                        fileContents = json.load(json_data)
+                    return fileContents["roastName"]
+                else:
+                    path = self.filePath(index)
+                    position = path.rfind("/")
+                    return path[position+1:]
+
+        return super(RecipeModel, self).data(index, role)
