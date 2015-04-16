57773e78de442ba7b1c5aa5d224339490dbf8263
CCoffie <CalebCoffie@gmail.com>
2015-04-16 16:48:24 -0400

    Beginning stages of recipe editting. Restructured how recipe steps are modified

--- roastero/modules/gui/CustomQtWidgets.py
+++ roastero/modules/gui/CustomQtWidgets.py
@@ -4,140 +4,24 @@ from PyQt5.QtCore import *
 import os, json
 
 class ComboBoxNoWheel(QComboBox):
+    def __init__(self, *args, **kwargs):
+        super(ComboBoxNoWheel, self).__init__()
+
     def wheelEvent (self, event):
         event.ignore()
 
 class TimeEditNoWheel(QTimeEdit):
+    def __init__(self, *args, **kwargs):
+        super(TimeEditNoWheel, self).__init__()
+
     def wheelEvent (self, event):
         event.ignore()
 
-class TableWidgetDragRows(QTableWidget):
-    def __init__(self, *args, **kwargs):
-        QTableWidget.__init__(self, *args, **kwargs)
-
-        self.setDragEnabled(True)
-        self.setAcceptDrops(True)
-        self.viewport().setAcceptDrops(True)
-        self.setDragDropOverwriteMode(False)
-        self.setDropIndicatorShown(True)
-
-        self.setSelectionMode(QAbstractItemView.SingleSelection)
-        self.setSelectionBehavior(QAbstractItemView.SelectRows)
-        self.setDragDropMode(QAbstractItemView.InternalMove)
-
-    def dropEvent(self, event):
-        if event.source() == self and (event.dropAction() == Qt.MoveAction or self.dragDropMode() == QAbstractItemView.InternalMove):
-            success, row, col, topIndex = self.dropOn(event)
-            if success:
-                selRows = self.getSelectedRowsFast()
-
-                top = selRows[0]
-                dropRow = row
-                if dropRow == -1:
-                    dropRow = self.rowCount()
-                offset = dropRow - top
-
-                for i, row in enumerate(selRows):
-                    r = row + offset
-                    if r > self.rowCount() or r < 0:
-                        r = 0
-                    self.insertRow(r)
-
-                selRows = self.getSelectedRowsFast()
-
-                top = selRows[0]
-                offset = dropRow - top
-                for i, row in enumerate(selRows):
-                    r = row + offset
-                    if r > self.rowCount() or r < 0:
-                        r = 0
-
-                    for j in range(self.columnCount()):
-                        if self.cellWidget(row, j):
-                            source = self.cellWidget(row, j)
-                            self.setCellWidget(r, j, source)
-                        else:
-                            source = QTableWidgetItem(self.item(row, j))
-                            self.setItem(r, j, source)
-                event.accept()
-
-        else:
-            QTableView.dropEvent(event)
-
-    def getSelectedRowsFast(self):
-        selRows = []
-        for item in self.selectionModel().selectedRows():
-            if item.row() not in selRows:
-                selRows.append(item.row())
-        return selRows
-
-    def droppingOnItself(self, event, index):
-        dropAction = event.dropAction()
-
-        if self.dragDropMode() == QAbstractItemView.InternalMove:
-            dropAction = Qt.MoveAction
-
-        if event.source() == self and event.possibleActions() & Qt.MoveAction and dropAction == Qt.MoveAction:
-            selectedIndexes = self.selectedIndexes()
-            child = index
-            while child.isValid() and child != self.rootIndex():
-                if child in selectedIndexes:
-                    return True
-                child = child.parent()
-
-        return False
-
-    def dropOn(self, event):
-        if event.isAccepted():
-            return False, None, None, None
-
-        index = QModelIndex()
-        row = -1
-        col = -1
-
-        if self.viewport().rect().contains(event.pos()):
-            index = self.indexAt(event.pos())
-            if not index.isValid() or not self.visualRect(index).contains(event.pos()):
-                index = self.rootIndex()
-
-        if self.model().supportedDropActions() & event.dropAction():
-            if index != self.rootIndex():
-                dropIndicatorPosition = self.position(event.pos(), self.visualRect(index), index)
-
-                if dropIndicatorPosition == QAbstractItemView.AboveItem:
-                    row = index.row()
-                    col = index.column()
-                    # index = index.parent()
-                elif dropIndicatorPosition == QAbstractItemView.BelowItem:
-                    row = index.row() + 1
-                    col = index.column()
-                    # index = index.parent()
-                else:
-                    row = index.row()
-                    col = index.column()
-
-            if not self.droppingOnItself(event, index):
-                return True, row, col, index
-
-        return False, None, None, None
-
-    def position(self, pos, rect, index):
-        r = QAbstractItemView.OnViewport
-        margin = 2
-        if pos.y() - rect.top() < margin:
-            r = QAbstractItemView.AboveItem
-        elif rect.bottom() - pos.y() < margin:
-            r = QAbstractItemView.BelowItem
-        elif rect.contains(pos, True):
-            r = QAbstractItemView.OnItem
-
-        if r == QAbstractItemView.OnItem and not (self.model().flags(index) & Qt.ItemIsDropEnabled):
-            r = QAbstractItemView.AboveItem if pos.y() < rect.center().y() else QAbstractItemView.BelowItem
-
-        return r
-
 class RecipeModel(QFileSystemModel):
     """A Subclass of QFileSystemModel to add a column"""
+    def __init__(self, *args, **kwargs):
+        super(RecipeModel, self).__init__()
+
     def columnCount(self, parent = QModelIndex()):
         return super(RecipeModel, self).columnCount()+1
 
--- roastero/modules/gui/RecipeEditorWindow.py
+++ roastero/modules/gui/RecipeEditorWindow.py
@@ -1,8 +1,10 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
-import json, time
-from ..gui.CustomQtWidgets import TimeEditNoWheel, ComboBoxNoWheel, TableWidgetDragRows
+import json, time, os
+from functools import partial
+from ..gui.CustomQtWidgets import TimeEditNoWheel, ComboBoxNoWheel
+from ..tools.fileNameFormatter import format_filename
 
 class RecipeEditor(QDialog):
     def __init__(self, recipeLocation=None):
@@ -20,16 +22,21 @@ class RecipeEditor(QDialog):
 
         self.create_ui()
 
+        self.recipe = {}
+        self.recipe["steps"] = [{'fanSpeed': 5, 'targetTemp': 150, 'sectionTime': 0}]
+
         if recipeLocation:
             self.load_recipe_file(recipeLocation)
             self.preload_recipe_information()
+        else:
+            self.preload_recipe_steps(self.recipeSteps)
 
     def create_ui(self):
         # Create main layout for window.
         self.layout = QGridLayout(self)
         self.layout.setRowStretch(1, 3)
 
-        # Create input fields.        
+        # Create input fields.
         self.create_input_fields()
         self.layout.addLayout(self.inputFieldLayout, 0, 0, 1, 2)
 
@@ -39,7 +46,7 @@ class RecipeEditor(QDialog):
 
         # Create Bottom Buttons.
         self.create_bottom_buttons()
-        self.layout.addLayout(self.bottomButtonLayout, 2, 0, 1, 2) 
+        self.layout.addLayout(self.bottomButtonLayout, 2, 0, 1, 2)
 
 
     def create_input_fields(self):
@@ -96,7 +103,7 @@ class RecipeEditor(QDialog):
         # Create labels for the edit boxes.
         recipeDescriptionBoxLabel = QLabel("Description: ")
         recipeStepsLabel = QLabel("Steps: ")
-        
+
         # Create widgets.
         self.recipeDescriptionBox = QTextEdit()
         self.recipeSteps = self.create_steps_spreadsheet()
@@ -106,32 +113,33 @@ class RecipeEditor(QDialog):
         self.bigEditLayout.addWidget(self.recipeDescriptionBox, 1, 0)
         self.bigEditLayout.addWidget(recipeStepsLabel, 0, 1)
         self.bigEditLayout.addWidget(self.recipeSteps, 1, 1)
-       
+
     def create_bottom_buttons(self):
         # Set bottom button layout.
         self.bottomButtonLayout = QHBoxLayout()
         self.bottomButtonLayout.setSpacing(0)
-        
+
         # Create buttons.
         self.saveButton = QPushButton("SAVE")
         self.closeButton = QPushButton("CLOSE")
-        
+
         # Assign object names to the buttons.
         self.saveButton.setObjectName("smallButton")
+        self.saveButton.clicked.connect(self.save_recipe)
         self.closeButton.setObjectName("smallButton")
         self.closeButton.clicked.connect(self.close_edit_window)
 
         # Create Spacer.
         self.spacer = QWidget()
         self.spacer.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        
+
         # Add widgets to the layout.
         self.bottomButtonLayout.addWidget(self.spacer)
         self.bottomButtonLayout.addWidget(self.closeButton)
         self.bottomButtonLayout.addWidget(self.saveButton)
 
     def create_steps_spreadsheet(self):
-        recipeStepsTable = TableWidgetDragRows()
+        recipeStepsTable = QTableWidget()
         recipeStepsTable.setShowGrid(False)
         recipeStepsTable.setAlternatingRowColors(True)
         recipeStepsTable.setCornerButtonEnabled(False)
@@ -140,7 +148,7 @@ class RecipeEditor(QDialog):
 
         # Steps spreadsheet
         recipeStepsTable.setColumnCount(4)
-        recipeStepsTable.setHorizontalHeaderLabels(["Temperature", "Fan Speed", "Section Time", "Reorder"])
+        recipeStepsTable.setHorizontalHeaderLabels(["Temperature", "Fan Speed", "Section Time", "Modify"])
 
         return recipeStepsTable
 
@@ -148,28 +156,32 @@ class RecipeEditor(QDialog):
         self.close()
 
     def preload_recipe_steps(self, recipeStepsTable):
+        steps = self.recipe["steps"]
+        self.load_recipe_steps(recipeStepsTable, steps)
+
+    def load_recipe_steps(self, recipeStepsTable, steps):
         # Create spreadsheet choices
         fanSpeedChoices = [str(x) for x in range(1,10)]
         targetTempChoices = ["Cooling"] + [str(x) for x in range(150, 501, 10)]
 
         # loop through recipe and load each step
-        for row in range(len(self.recipe["steps"])):
+        for row in range(len(steps)):
             recipeStepsTable.insertRow(recipeStepsTable.rowCount())
             # Temperature Value
             sectionTempWidget = ComboBoxNoWheel()
             sectionTempWidget.addItems(targetTempChoices)
             sectionTempWidget.insertSeparator(1)
-            if 'targetTemp' in self.recipe["steps"][row]:
-                sectionTemp = self.recipe["steps"][row]["targetTemp"]
+            if 'targetTemp' in steps[row]:
+                sectionTemp = steps[row]["targetTemp"]
                 # Accommodate for temperature not fitting in 10 increment list
-                if str(self.recipe["steps"][row]["targetTemp"]) in targetTempChoices:
-                    sectionTempWidget.setCurrentIndex(targetTempChoices.index(str(self.recipe["steps"][row]["targetTemp"]))+1)
+                if str(steps[row]["targetTemp"]) in targetTempChoices:
+                    sectionTempWidget.setCurrentIndex(targetTempChoices.index(str(steps[row]["targetTemp"]))+1)
                 else:
-                    roundedNumber = self.recipe["steps"][row]["targetTemp"] - (self.recipe["steps"][row]["targetTemp"] % 10)
-                    sectionTempWidget.insertItem(targetTempChoices.index(str(roundedNumber))+2, str(self.recipe["steps"][row]["targetTemp"]))
+                    roundedNumber = steps[row]["targetTemp"] - (steps[row]["targetTemp"] % 10)
+                    sectionTempWidget.insertItem(targetTempChoices.index(str(roundedNumber))+2, str(steps[row]["targetTemp"]))
                     sectionTempWidget.setCurrentIndex(targetTempChoices.index(str(roundedNumber))+2)
 
-            elif 'cooling' in self.recipe["steps"][row]:
+            elif 'cooling' in steps[row]:
                 sectionTemp = "Cooling"
                 sectionTempWidget.setCurrentIndex(targetTempChoices.index("Cooling"))
 
@@ -178,29 +190,54 @@ class RecipeEditor(QDialog):
             sectionTimeWidget = TimeEditNoWheel()
             sectionTimeWidget.setDisplayFormat("mm:ss")
             # Set QTimeEdit to the right time from recipe
-            sectionTimeStr = time.strftime("%M:%S", time.gmtime(self.recipe["steps"][row]["sectionTime"]))
+            sectionTimeStr = time.strftime("%M:%S", time.gmtime(steps[row]["sectionTime"]))
             sectionTime = QTime().fromString(sectionTimeStr, "mm:ss")
             sectionTimeWidget.setTime(sectionTime)
 
             # Fan Speed Value
             sectionFanSpeedWidget = ComboBoxNoWheel()
             sectionFanSpeedWidget.addItems(fanSpeedChoices)
-            sectionFanSpeedWidget.setCurrentIndex(fanSpeedChoices.index(str(self.recipe["steps"][row]["fanSpeed"])))
+            sectionFanSpeedWidget.setCurrentIndex(fanSpeedChoices.index(str(steps[row]["fanSpeed"])))
 
-            # Move QIcon
-            moveIconItem = QTableWidgetItem()
-            moveIconItem.setIcon(QIcon('modules/gui/images/downArrow.png'))
+            # Move field
+            upArrow = QPushButton()
+            upArrow.setIcon(QIcon('modules/gui/images/upArrow.png'))
+            upArrow.clicked.connect(partial(self.move_recipe_step_up, row))
+            downArrow = QPushButton()
+            downArrow.setIcon(QIcon('modules/gui/images/downArrow.png'))
+            downArrow.clicked.connect(partial(self.move_recipe_step_down, row))
+
+            deleteRow = QPushButton("X")
+            deleteRow.clicked.connect(partial(self.delete_recipe_step, row))
+
+            insertRow = QPushButton("+")
+            insertRow.clicked.connect(partial(self.insert_recipe_step, row))
+
+            modifyRowWidgetLayout = QGridLayout()
+            modifyRowWidgetLayout.setSpacing(0)
+            modifyRowWidgetLayout.addWidget(upArrow, 0, 0)
+            modifyRowWidgetLayout.addWidget(downArrow, 1, 0)
+            modifyRowWidgetLayout.addWidget(deleteRow, 0, 1)
+            modifyRowWidgetLayout.addWidget(insertRow, 1, 1)
+
+            modifyRowWidget = QWidget()
+            modifyRowWidget.setLayout(modifyRowWidgetLayout)
+
+            # print(modifyRowWidget.layout())
+            # moveIconItem = QTableWidgetItem()
+            # moveIconItem.setIcon(QIcon('modules/gui/images/downArrow.png'))
 
             # Add widgets
             recipeStepsTable.setCellWidget(row, 0, sectionTempWidget)
             recipeStepsTable.setCellWidget(row, 1, sectionFanSpeedWidget)
             recipeStepsTable.setCellWidget(row, 2, sectionTimeWidget)
-            recipeStepsTable.setItem(row, 3, moveIconItem)
+            recipeStepsTable.setCellWidget(row, 3, modifyRowWidget)
 
     def load_recipe_file(self, recipeFile):
         # Load recipe file
         recipeFileHandler = open(recipeFile)
         self.recipe = json.load(recipeFileHandler)
+        self.recipe["file"] = recipeFile
         recipeFileHandler.close()
 
     def preload_recipe_information(self):
@@ -214,3 +251,110 @@ class RecipeEditor(QDialog):
         self.recipeDescriptionBox.setText(self.recipe["roastDescription"]["description"])
 
         self.preload_recipe_steps(self.recipeSteps)
+
+    def move_recipe_step_up(self, row):
+        if row != 0:
+            steps = self.get_current_table_values()
+            newSteps = steps
+
+            # Swap the steps
+            newSteps[row], newSteps[row-1] = newSteps[row-1], newSteps[row]
+
+            # Rebuild table with new steps
+            self.rebuild_recipe_steps_table(newSteps)
+
+    def move_recipe_step_down(self, row):
+        if row != self.recipeSteps.rowCount()-1:
+            steps = self.get_current_table_values()
+            newSteps = steps
+
+            # Swap the steps
+            newSteps[row], newSteps[row+1] = newSteps[row+1], newSteps[row]
+
+            # Rebuild table with new steps
+            self.rebuild_recipe_steps_table(newSteps)
+
+    def delete_recipe_step(self, row):
+        steps = self.get_current_table_values()
+        newSteps = steps
+
+        # Delete step
+        newSteps.pop(row)
+
+        # Rebuild table with new steps
+        self.rebuild_recipe_steps_table(newSteps)
+
+    def insert_recipe_step(self, row):
+        steps = self.get_current_table_values()
+        newSteps = steps
+
+        # insert step
+        newSteps.insert(row+1, {'fanSpeed': 5, 'targetTemp': 150, 'sectionTime': 0})
+
+        # Rebuild table with new steps
+        self.rebuild_recipe_steps_table(newSteps)
+
+    def get_current_table_values(self):
+        recipeSteps = []
+        for row in range(0, self.recipeSteps.rowCount()):
+            currentRow = {}
+            currentRow["sectionTime"] = QTime(0, 0, 0).secsTo(self.recipeSteps.cellWidget(row, 2).time())
+            currentRow["fanSpeed"] = int(self.recipeSteps.cellWidget(row, 1).currentText())
+
+            # Get Temperature or cooling
+            if self.recipeSteps.cellWidget(row, 0).currentText() == "Cooling":
+                currentRow["cooling"] = True
+            else:
+                currentRow["targetTemp"] = int(self.recipeSteps.cellWidget(row, 0).currentText())
+
+            recipeSteps.append(currentRow)
+
+        # Return copied rows
+        return recipeSteps
+
+    def rebuild_recipe_steps_table(self, newSteps):
+        if len(newSteps) < 1:
+            alert = QMessageBox()
+            alert.setWindowTitle('Roastero')
+            alert.setStyleSheet(self.style)
+            alert.setText("You must have atleast one step!")
+            alert.exec_()
+
+        else:
+            # Delete all the current rows
+            while self.recipeSteps.rowCount() > 0:
+                self.recipeSteps.removeRow(0)
+            # Add the new step sequence
+            self.load_recipe_steps(self.recipeSteps, newSteps)
+
+    def save_recipe(self):
+        # Determine Recipe File Name
+        if "file" in self.recipe:
+            filePath = self.recipe["file"]
+        else:
+            filePath = os.path.abspath("./recipes/local") + "/" + format_filename(self.recipeName.text()) + ".json"
+            # TODO: Account for existing file with same name
+
+        # Create Dictionary with all the new recipe information
+        self.newRecipe = {}
+        self.newRecipe["roastName"] = self.recipeName.text()
+        self.newRecipe["steps"] = self.get_current_table_values()
+        self.newRecipe["roastDescription"] = {}
+        self.newRecipe["roastDescription"]["roastType"] = self.recipeRoastType.text()
+        self.newRecipe["roastDescription"]["description"] = self.recipeDescriptionBox.toPlainText()
+        self.newRecipe["creator"] = self.recipeCreator.text()
+        self.newRecipe["bean"] = {}
+        self.newRecipe["bean"]["region"] = self.beanRegion.text()
+        self.newRecipe["bean"]["country"] = self.beanCountry.text()
+        self.newRecipe["bean"]["source"] = {}
+        self.newRecipe["bean"]["source"]["reseller"] = self.beanStore.text()
+        self.newRecipe["bean"]["source"]["link"] = self.beanLink.text()
+        self.newRecipe["totalTime"] = 0
+        for step in self.newRecipe["steps"]:
+            self.newRecipe["totalTime"] += step["sectionTime"]
+
+        # Write the recipe to a file
+        jsonObject = json.dumps(self.newRecipe, indent=4)
+        file = open(filePath, 'w')
+        file.write(jsonObject)
+        file.close()
