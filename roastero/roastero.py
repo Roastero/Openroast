baaf62ef8f03dfbd8eb012dcbe8de3d33f584318
Mark Spicer <spicer93@gmail.com>
2015-05-22 11:08:22 -0400

    Attempted to comment and clean main file.

--- roastero/roastero.py
+++ roastero/roastero.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
+# Authors: Mark Spicer, Caleb Coffie
+# License: Roastero is released under GPLv3. Please see the license file for a
+#          copy of the license.
+# Contact: For additional information, please email admin@roastero.com
 
 # Standard Library Imports
 import sys, os, shutil, inspect
 
-# Change execution directory to file location
 def get_script_dir(follow_symlinks=True):
+    """ Checks where the script is being executed from to verify the imports
+    will work properly. """
     if getattr(sys, 'frozen', False): # py2exe, PyInstaller, cx_Freeze
         path = os.path.abspath(sys.executable)
     else:
@@ -13,6 +18,7 @@ def get_script_dir(follow_symlinks=True):
         path = os.path.realpath(path)
     return os.path.dirname(path)
 
+# Change execution directory to file location before importing PyQt.
 os.chdir(get_script_dir())
 
 # PyQt imports
@@ -23,14 +29,18 @@ from PyQt5.QtGui import QFontDatabase
 from modules.gui.MainWindow import MainWindow
 
 def check_for_user_folder():
+    """ Verify that a user folder exists and create one if it does not before
+    launching the application """
     roasteroUserFolder = os.path.expanduser('~/Documents/Roastero/')
     if not os.path.isdir(roasteroUserFolder):
         shutil.copytree("recipes", os.path.join(roasteroUserFolder, "recipes"))
         shutil.copytree("log", os.path.join(roasteroUserFolder, "log"))
         shutil.copyfile("config.ini", roasteroUserFolder)
 
+# Check for user folder.
 check_for_user_folder()
 
+# Create application.
 app = QApplication(sys.argv)
 
 # Set application style.
@@ -41,6 +51,7 @@ QFontDatabase.addApplicationFont("static/fonts/asap/asap-italic.ttf")
 style = open('static/mainStyle.css').read()
 QApplication.setStyleSheet(app, style)
 
+# Create main window and show.
 window = MainWindow()
 window.show()
 sys.exit(app.exec_())
